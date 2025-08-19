6b92a7c3035dfe8b0b100f991962f56e3c017bfe
Will Childs-Klein <childw@amazon.com>
2025-08-19 14:51:48 -0400

    Benchmark SHA3 on mac

--- csrc/sha3_512.cpp
+++ csrc/sha3_512.cpp
@@ -1,6 +1,8 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 #include <openssl/digest.h>
-#define DIGEST_NAME sha3_512
-#define DIGEST_LENGTH 64
+#define DIGEST_NAME       sha3_512
+#define DIGEST_LENGTH     64
 #define DIGEST_BLOCK_SIZE 72
-#define MD_CTX_SIZE 400
+#define MD_CTX_SIZE       400
 #include "hash_evp_template.cpp.template"
